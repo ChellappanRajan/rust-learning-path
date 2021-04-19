0105598090f9af3ca77f2c3ef770a583aa8c2cc2
Chellappan <vrchellappan@gmail.com>
2021-04-19 18:14:04 +0530

    feat: shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -10,4 +10,14 @@ fn main() {
 
     a_number = 20;
     println!("the mutated value {}", a_number);
+
+    //Shadowing
+    //In rust delcaring new varibale with same name as previous variable name,which 
+    //creates a new bindin, In rust this operation is called shadowing.
+    let number = 5;
+    let number = number + 5;
+    let number = 5 * 5;
+
+    println!("the number is {}", number);
+
 }
