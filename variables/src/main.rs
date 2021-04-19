97af628c646ae0bf2310f60ba5a6a174998d8653
Chellappan <vrchellappan@gmail.com>
2021-04-19 18:08:19 +0530

    feat: create and use varaibles

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a_number = 10;
+    let a_string = "Hello world";
+    let a_boolean = true;
+
+    println!("the number is {}",a_number);
+    println!("the boolean is {}", a_boolean);
+    println!("the string is {}", a_string);
+}
