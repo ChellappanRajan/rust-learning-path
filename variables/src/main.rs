eebb7a60191f906628382dd7aa105fc6feb36a09
Chellappan <vrchellappan@gmail.com>
2021-04-19 18:10:34 +0530

    feat: mut keyword to mutate variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let a_number = 10;
+    let mut a_number = 10;
     let a_string = "Hello world";
     let a_boolean = true;
 
     println!("the number is {}",a_number);
     println!("the boolean is {}", a_boolean);
     println!("the string is {}", a_string);
+
+
+    a_number = 20;
+    println!("the mutated value {}", a_number);
 }
